3fbfc3b6c49fb3ee48b7289db70b919c08ad7076
Karen Ambartsumov <k.ambartsumov@gmail.com>
2026-06-02 22:39:59 +0300

    Исправлена ошибка конфига биндинга локалхоста

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,11 @@ import { fileURLToPath, URL } from 'node:url';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '127.0.0.1',
+    port: 5173,
+    strictPort: true,
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
